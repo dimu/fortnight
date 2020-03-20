bd586803eabb0c6e4530b7c2759ee50c4aec8edb
dimu <wenxiang0705@qq.com>
2020-03-20 23:07:35 +0800

    add jmm final example

--- java/basic/src/main/java/access/SingleImport.java
+++ java/basic/src/main/java/access/SingleImport.java
@@ -4,7 +4,22 @@ package access;
 import java.util.ArrayList;
 
 public class SingleImport {
+
+  /**
+   * 要么初始化要么通过构造函数赋值
+   */
+  public final int COUNT ;
+
   public static void main(String[] args) {
     ArrayList list = new java.util.ArrayList();
+
   }
+
+  public  SingleImport(int count) {
+    /**
+     * 通过构造函数初始化final域变量
+     */
+    this.COUNT = count;
+  }
+
 } ///:~
