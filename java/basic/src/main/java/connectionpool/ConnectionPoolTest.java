75585a93a6d98209c70a0d1eacbb6164aef9a5e5
dimu <wenxiang0705@qq.com>
2020-03-28 22:04:57 +0800

    add simple connection pool res

--- java/basic/src/main/java/connectionpool/ConnectionPoolTest.java
+++ java/basic/src/main/java/connectionpool/ConnectionPoolTest.java
@@ -3,6 +3,7 @@ package connectionpool;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * connection pool test ,to see the performance enhance
@@ -11,47 +12,84 @@ import java.util.concurrent.CountDownLatch;
  */
 public class ConnectionPoolTest {
 
+    public static int thread_size = 100;
+
     static CountDownLatch start = new CountDownLatch(1);
 
     /**
      * the pool size
      */
-    static CountDownLatch end = new CountDownLatch(10);
+    static CountDownLatch end = new CountDownLatch(thread_size);
 
-    public static void main(String[] args) {
+    public static ConnectionPool connectionPool = new ConnectionPool(10);
+
+    public static void main(String[] args) throws InterruptedException {
 
         /**
          * the initial number of connection pool : 10
          */
-        ConnectionPool connectionPool = new ConnectionPool(10);
+        int repeatCount = 20;
+        AtomicInteger getCount = new AtomicInteger();
+        AtomicInteger notGetCount = new AtomicInteger();
 
+        for (int i = 0; i < thread_size; i++) {
+            Thread thread = new Thread( new ConnectionClient(repeatCount, getCount, notGetCount) );
+            thread.start();
+        }
+
+        start.countDown();
+        end.await();
+
+        System.out.println("total invoke " + (repeatCount * thread_size));
+        System.out.println("get connection: " + getCount );
+        System.out.println("not get connection: " + notGetCount);
 
     }
 
-    class ConnectionClient implements Runnable {
+    static class ConnectionClient implements Runnable {
 
-        ConnectionPool connectionPool;
+        private int repeatTime;
 
-        public ConnectionClient(ConnectionPool connectionPool) {
-            this.connectionPool = connectionPool;
+        private AtomicInteger getCount;
+
+        private AtomicInteger notGetCount;
+
+        public ConnectionClient(int repeatTime, AtomicInteger getCount, AtomicInteger notGetCount) {
+            this.repeatTime = repeatTime;
+            this.getCount = getCount;
+            this.notGetCount = notGetCount;
         }
 
         @Override
         public void run() {
-            Connection connection = null;
+
             try {
                 start.await();
-                connection = connectionPool.fetchConnection(1000);
-                connection.commit();
-            } catch (SQLException e) {
-                e.printStackTrace();
             } catch (InterruptedException e) {
                 e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                connectionPool.releaseConnection(connection);
             }
+
+            while (repeatTime > 0 ) {
+                try {
+                    Connection connection = connectionPool.fetchConnection(1000);
+                    if (null != connection) {
+                        try {
+                            connection.createStatement();
+                            connection.commit();
+                        } finally {
+                            connectionPool.releaseConnection(connection);
+                            getCount.incrementAndGet();
+                        }
+                    } else {
+                        notGetCount.incrementAndGet();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    repeatTime--;
+                }
+            }
+            end.countDown();
 
         }
     }
