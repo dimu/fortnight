315163cd994797985a4cd60168d762200d5141f1
dimu <wenxiang0705@qq.com>
2020-04-15 11:20:15 +0800

    add snowflake research

---
+++ java/basic/src/main/java/distributed/OnlyPrimarykeyProducer.java
@@ -0,0 +1,118 @@
+package distributed;
+
+/**
+ * 尝试使用雪花算法(SnowFlake)实现分布式唯一主键
+ * 由推特开源公布
+ * 0 - 0000000000 0000000000 0000000000 0000000000 0 - 00000 - 00000 - 000000000000
+ * 第一位为未使用，接下来的41位为毫秒级时间(41位的长度可以使用69年)，然后是5位datacenterId和5位workerId(10位的长度最多支持部署1024个节点） ，
+ * 最后12位是毫秒内的计数（12位的计数顺序号支持每个节点每毫秒产生4096个ID序号）
+ *
+ * 一共加起来刚好64位，为一个Long型。(转换成字符串长度为18)
+ * @author dwx
+ */
+public class OnlyPrimarykeyProducer {
+
+    //起始时间戳  2020-01-01 00:00:00
+    private final static long START_STMP = 1577808000000L;
+
+    //序列号占用的位数
+    private final static long SEQUENCE_BIT = 12;
+
+    //机器标识占用的位数
+    private final static long MACHINE_BIT = 5;
+
+    //数据中心占用的位数
+    private final static long DATACENTER_BIT = 5;
+
+    /**
+     * 每一部分的最大值
+     */
+    private final static long MAX_DATACENTER_NUM = -1L ^ (-1L << DATACENTER_BIT);
+    private final static long MAX_MACHINE_NUM = -1L ^ (-1L << MACHINE_BIT);
+    private final static long MAX_SEQUENCE = -1L ^ (-1L << SEQUENCE_BIT);
+
+    /**
+     * 每一部分向左的位移
+     */
+    private final static long MACHINE_LEFT = SEQUENCE_BIT;
+    private final static long DATACENTER_LEFT = SEQUENCE_BIT + MACHINE_BIT;
+    private final static long TIMESTMP_LEFT = DATACENTER_LEFT + DATACENTER_BIT;
+
+    //数据中心
+    private long datacenterId;
+
+    //机器标识
+    private long machineId;
+
+    //序列号
+    private long sequence = 0L;
+
+    //上一次时间戳
+    private long lastStmp = -1L;
+
+    public OnlyPrimarykeyProducer(long datacenterId, long machineId) {
+        if (datacenterId > MAX_DATACENTER_NUM || datacenterId < 0) {
+            throw new IllegalArgumentException("datacenterId can't be greater than MAX_DATACENTER_NUM or less than 0");
+        }
+        if (machineId > MAX_MACHINE_NUM || machineId < 0) {
+            throw new IllegalArgumentException("machineId can't be greater than MAX_MACHINE_NUM or less than 0");
+        }
+        this.datacenterId = datacenterId;
+        this.machineId = machineId;
+    }
+
+    /**
+     * 产生下一个ID
+     *
+     * @return
+     */
+    public synchronized long nextId() {
+        long currStmp = getNewStamp();
+        if (currStmp < lastStmp) {
+            throw new RuntimeException("Clock moved backwards.  Refusing to generate id");
+        }
+
+        if (currStmp == lastStmp) {
+            //相同毫秒内，序列号自增
+            sequence = (sequence + 1) & MAX_SEQUENCE;
+            //同一毫秒的序列数已经达到最大
+            if (sequence == 0L) {
+                currStmp = getNextMill();
+            }
+        } else {
+            //不同毫秒内，序列号置为0
+            sequence = 0L;
+        }
+
+        lastStmp = currStmp;
+
+        //时间戳部分 | 数据中心部分 | 机器标识部分 | 序列号部分
+        return (currStmp - START_STMP) << TIMESTMP_LEFT
+                | datacenterId << DATACENTER_LEFT
+                | machineId << MACHINE_LEFT
+                | sequence;
+    }
+
+    private long getNextMill() {
+        long mill = getNewStamp();
+        while (mill <= lastStmp) {
+            mill = getNewStamp();
+        }
+        return mill;
+    }
+
+    private long getNewStamp() {
+        return System.currentTimeMillis();
+    }
+
+    public static void main(String[] args) {
+        OnlyPrimarykeyProducer snowFlake = new OnlyPrimarykeyProducer(2, 3);
+
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1000000; i++) {
+            System.out.println(snowFlake.nextId());
+        }
+
+        System.out.println(System.currentTimeMillis() - start);
+    }
+}
