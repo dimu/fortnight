b7bc1dcfb979ef7080428814445e6306285108e7
dimu <wenxiang0705@qq.com>
2020-06-01 12:20:17 +0800

    add caffeine test

--- java/basic/src/main/java/cache/CaffeineTest.java
+++ java/basic/src/main/java/cache/CaffeineTest.java
@@ -1,6 +1,11 @@
 package cache;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import javafx.util.Duration;
 import org.junit.Test;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -8,7 +13,16 @@ import org.junit.Test;
 public class CaffeineTest {
 
     @Test
-    public void writeTest() {
+    public void writeTest() throws InterruptedException {
+        Cache<String, String> cache = Caffeine.newBuilder().expireAfterWrite(10, TimeUnit.SECONDS)
+//                .refreshAfterWrite(Duration.seconds(30))
+                .initialCapacity(10)
+                .maximumSize(100)
+                .build();
+        cache.put("name", "dwx");
+        System.out.println(cache.getIfPresent("name"));
+        TimeUnit.SECONDS.sleep(20);
+        System.out.println(cache.getIfPresent("name"));
 
     }
 }
